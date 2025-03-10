2b56b97453b1970477f8ed8f63c82438c8f553f4
Valentin Ignatev <valentignatev@gmail.com>
2025-03-10 15:25:37 +0100

    horizontal scroll and update emacs to 30.1

--- early-init.el
+++ early-init.el
@@ -1,5 +1,13 @@
 ;; -*- lexical-binding: t; -*-
 
+;; Hack to reduce startup flashbang (it doesn't fully solve it, but makes it slightly better)
+(setq frame-inhibit-implied-resize t)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(background-color . "#052525") default-frame-alist)
+(push '(foreground-color . "#ffffff") default-frame-alist)
+
 (setq gc-cons-threshold most-positive-fixnum)
 
 ;; Disable package.el
--- init.el
+++ init.el
@@ -35,8 +35,10 @@
       eldoc-echo-area-use-multiline-p nil
       pixel-scroll-precision-interpolate-page t
       make-backup-files nil
+      mouse-wheel-tilt-scroll t
       warning-minimum-level :error
       gdb-many-windows t)
+
 (setq-default select-active-regions nil
               indent-tabs-mode nil
               truncate-lines t
@@ -54,7 +56,7 @@
 
 ;; Font settings
 (defvar font-name "Hack")
-(defvar font-size (if IS-WINDOWS 14 18))
+(defvar font-size (if IS-WINDOWS 14 17))
 (set-frame-font (format "%s-%d" font-name font-size) t t)
 
 (column-number-mode 1)
@@ -155,12 +157,12 @@ Support for more interface parts will be added as I feel like it"
             (my/scale-interface old-frame-dpi current-frame-dpi)))))
 
 ;; elpaca boilerplate
-(defvar elpaca-installer-version 0.7)
+(defvar elpaca-installer-version 0.10)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil :depth 1
+                              :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
@@ -170,20 +172,20 @@ Support for more interface parts will be added as I feel like it"
   (add-to-list 'load-path (if (file-exists-p build) build repo))
   (unless (file-exists-p repo)
     (make-directory repo t)
-    (when (< emacs-major-version 28) (require 'subr-x))
+    (when (<= emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
-        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
-                                                 ,@(when-let ((depth (plist-get order :depth)))
-                                                     (list (format "--depth=%d" depth) "--no-single-branch"))
-                                                 ,(plist-get order :repo) ,repo))))
-                 ((zerop (call-process "git" nil buffer t "checkout"
-                                       (or (plist-get order :ref) "--"))))
-                 (emacs (concat invocation-directory invocation-name))
-                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                 ((require 'elpaca))
-                 ((elpaca-generate-autoloads "elpaca" repo)))
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
             (progn (message "%s" (buffer-string)) (kill-buffer buffer))
           (error "%s" (with-current-buffer buffer (buffer-string))))
       ((error) (warn "%s" err) (delete-directory repo 'recursive))))
@@ -197,7 +199,7 @@ Support for more interface parts will be added as I feel like it"
 ;; basically ensure elpaca is loaded before continuing
 (elpaca-wait)
 (elpaca bind-key)
-(elpaca compat :demant t)
+;; (elpaca compat :demand t)
 
 (elpaca use-package :demand t)
 
@@ -265,9 +267,12 @@ Support for more interface parts will be added as I feel like it"
          ("\\.c\\'" . simpc-mode)
          ("\\.h\\'" . simpc-mode)
          ("\\.html\\'" . html-mode)
-         ("\\.env\\'" . conf-mode))
+         ("\\.env\\'" . conf-mode)
+         ("\\.env.local\\'" . conf-mode))
   :bind (("C-x t" . my/terminal-in-project-root)
-         ("<f7>" . recompile))
+         ("<f7>" . recompile)
+         ("S-<wheel-up>" . (lambda () (interactive) (scroll-right 5)))
+         ("S-<wheel-down>" . (lambda () (interactive) (scroll-left 5))))
   :hook ((text-mode-hook . visual-line-mode)
          (org-mode-hook . visual-line-mode)
          (compilation-mode-hook . visual-line-mode)
@@ -336,6 +341,10 @@ Support for more interface parts will be added as I feel like it"
 (use-package evil-surround
   :after evil
   :config (global-evil-surround-mode 1))
+
+(use-package evil-exchange
+  :after evil
+  :config (evil-exchange-install))
 
 (use-package vertico
   ;; :straight (:files (:defaults "extensions/*"))
@@ -637,7 +646,58 @@ source: https://stackoverflow.com/questions/24356401/how-to-append-multiple-elem
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bc137acdbe14673fbba58c340a1d530055df1ef1624f83c479364832f2653cb3" "9d8d60e69db09062e759f008e59fac1d76618b09cddaf06cef093653c16f1e75" "ead62296b48539886f4abbefa54c7a3b294144d62fed60ee3cae08a2b1c1f0cf" "661c38fdd381265206a1d74a765ef1a219ed071c26082547aaacbb92064ffbf1" "077f3e88cee64127903484d97a54dbecc40f4f6b4b7e266b1023991d2c31d25a" "342ed922b461613b79c29624c4bed600466bef9270b0763bb81bcf7c7a011bea" "5d05e8db28c566fe2a4e8bd87a17bf89bbaddbdbe76fc15e1f67b1a61f80c621" "4edad12267c88bb57aab5a5c0d2e23740c6f552b6a36fb785dfb4e4725808eab" "9f986dcc0de26c1c8b6dfd749eb7351b1a3c8db31b3330a7dfdd25be1b47cb22" "a32c59022bb35d2f7b56960c2c5b8b07ec5b9a66c2a47de58f19ec74b834862e" "372905daccda4574b28e5d5b64d5a4059da9e3c5548abc274af04fe63adc1372" "9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2" "1594eb8fc081be254c7df7b2a37e9808f79c94863366da6d34bbe735519a30f5" "1d6b446390c172036395b3b87b75321cc6af7723c7545b28379b46cc1ae0af9e" "e410458d3e769c33e0865971deb6e8422457fad02bf51f7862fa180ccc42c032" "b0cbcb2fa0c69ab36f4505fec9967969b73327f1b8564f9afface8afd216bc93" "8d146df8bd640320d5ca94d2913392bc6f763d5bc2bb47bed8e14975017eea91" "99d1e29934b9e712651d29735dd8dcd431a651dfbe039df158aa973461af003e" "fef6645175d4c5f9d573daca2ba4d7efa781be10967024d1d8e6ef0c4aa71437" "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+   '("ae20535e46a88faea5d65775ca5510c7385cbf334dfa7dde93c0cd22ed663ba0"
+     "72b4e4ad13ff8a62e0426cffbe50939736ac1bac09af753b4da1b28ec99186ec"
+     "ad5727801b610ff292f7f8833b47fa35ad18d461023d992497f3fcc10a7b164a"
+     "120daecf822f5af47ec6c03ea654e722ff86135970a3b5d774f12f329f930bb5"
+     "c451656eb8440a62dd2a3b140a8516647d39b912da1262fc1779f8f2bcc63925"
+     "acaa346f0baf7d9f6e04bb3cbfce0657817b39db333e612a7a12a34e5f2d04d5"
+     "4b59396c6a4cc8ca75c3addb4ee1021af6dc69e2f15955a39f055df28cf3f81e"
+     "f92f07aaf462d3b6ff0f054c55a5bde605828bbb66180c24b6ded204e2206863"
+     "f90edafccd777cd3df7b88bb5bb72931d8cbd5ef92423fbca0c3bc344e03f1e2"
+     "ecc2e41ce0e5b6671dd1f43478b9be35fb0fe811d25d20c46df68472d6f623e6"
+     "de4382be014e101c135c3d7c638a488e5cb843fa61554008f08e7bec1bc1abb6"
+     "48cfb7c9e5ad83901f9789790a7ae427ab4242005c2ee6d0b0f0b5ff5e3f724e"
+     "0924b7ae3f77f1eb29ea6a3bed73539ac4b26e2e60c4fd51ad00196df43005c5"
+     "16155a11eea450abb13a82e636bbe9e216966898cecbec7a7328ca25ac2e1767"
+     "4861ed05311cce238097dc81a922d28c291c3073512d2a2d9a36dc3855dadc5c"
+     "af4e265175af859104e4db52e7b16b95d6bc16c2c48ec43462c68b314795c8bf"
+     "f8cbad74cca5b307522e5078709cafeff74ef789e5be469b0956c027aef62fdf"
+     "7762a9a98d1c7e1f3eaabeb743f16aa016f39e4e3b20ac40e04fcd7475aae866"
+     "6ed2a90b13f77bac6b75f36352d72378b2a17346518884675aa56339a3bc38a9"
+     "d5d3915d6a61fc9cd5c8b3d2cce73857fc4d8eb18b944964422341cb78d693e8"
+     "9ddb83c12595e789e9abd04a5c0705661748776223a794a6f64669352b956e79"
+     "1b786d3723357a41cbb42c0a9f7e000e3f8c1c488691c0e92b7c1c69d49bf169"
+     "4c33bd6d84f2155c63aca2f49b6564f616030e32bddef1cf41e34ff70a4f3eb2"
+     "6b91ddbc12b0fc9f43fb9e31be8b76b16e387312232324c48e8c666418fe643c"
+     "d9a947788a4c5f7051c4ad3a3e0e9d76218209899683d3e9ed1e2aa6cd10d462"
+     "70b0a307c84ae939e8700fe0b2d45043103f4cd03fde8ed69ccee6b2f164dd43"
+     "0279c1b81b569e46a4ee8e87e5e381568bf931257287191fd091f2948f7e2e8e"
+     "48bf9307e1468f596789717043cc5a332ce30310cbb747ae0d2ce736ff23538f"
+     "bc137acdbe14673fbba58c340a1d530055df1ef1624f83c479364832f2653cb3"
+     "9d8d60e69db09062e759f008e59fac1d76618b09cddaf06cef093653c16f1e75"
+     "ead62296b48539886f4abbefa54c7a3b294144d62fed60ee3cae08a2b1c1f0cf"
+     "661c38fdd381265206a1d74a765ef1a219ed071c26082547aaacbb92064ffbf1"
+     "077f3e88cee64127903484d97a54dbecc40f4f6b4b7e266b1023991d2c31d25a"
+     "342ed922b461613b79c29624c4bed600466bef9270b0763bb81bcf7c7a011bea"
+     "5d05e8db28c566fe2a4e8bd87a17bf89bbaddbdbe76fc15e1f67b1a61f80c621"
+     "4edad12267c88bb57aab5a5c0d2e23740c6f552b6a36fb785dfb4e4725808eab"
+     "9f986dcc0de26c1c8b6dfd749eb7351b1a3c8db31b3330a7dfdd25be1b47cb22"
+     "a32c59022bb35d2f7b56960c2c5b8b07ec5b9a66c2a47de58f19ec74b834862e"
+     "372905daccda4574b28e5d5b64d5a4059da9e3c5548abc274af04fe63adc1372"
+     "9a977ddae55e0e91c09952e96d614ae0be69727ea78ca145beea1aae01ac78d2"
+     "1594eb8fc081be254c7df7b2a37e9808f79c94863366da6d34bbe735519a30f5"
+     "1d6b446390c172036395b3b87b75321cc6af7723c7545b28379b46cc1ae0af9e"
+     "e410458d3e769c33e0865971deb6e8422457fad02bf51f7862fa180ccc42c032"
+     "b0cbcb2fa0c69ab36f4505fec9967969b73327f1b8564f9afface8afd216bc93"
+     "8d146df8bd640320d5ca94d2913392bc6f763d5bc2bb47bed8e14975017eea91"
+     "99d1e29934b9e712651d29735dd8dcd431a651dfbe039df158aa973461af003e"
+     "fef6645175d4c5f9d573daca2ba4d7efa781be10967024d1d8e6ef0c4aa71437"
+     "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af"
+     "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf"
+     "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088"
+     "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"
+     default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
